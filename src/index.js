d451133c8988467546ed3b0696fc7129b522b8b3
ruredot <spanziao@gmail.com>
2023-06-15 10:39:22 +0900

    fix: 중복된 함수명uploadVideo -> handleVideoUpload

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ databaseService.connection.connect();
 app.use(express.json());
 
 // 동영상 업로드 및 비교를 위한 라우트 핸들러 등록
-app.post('/upload', videoController.uploadVideo);
+app.post('/upload', videoController.handleVideoUpload);
 //app.post('/compare', videoController.compareVideo);
 
 // 로그인을 위한 라우트 핸들러 등록
